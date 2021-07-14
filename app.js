c6cbc788070bc774e0449bcf210627ddade33e88
djghis <djghis@hotmail.com>
2021-07-14 17:23:43 +0100

    change app.js to test new manual review of the aws pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Gg is becoming a DevOps'));
+app.get('/', (req, res) => res.send('Gg is becoming a DevOps, and adjusting the Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
